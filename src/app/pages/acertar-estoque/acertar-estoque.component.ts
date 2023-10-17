f59b7bf82ff8751f3a4649414b4ea1046f719a1f
matheusgmm <matheus.gmuccio@gmail.com>
2023-10-17 12:42:34 -0300

    Feat: Adicionando função para acertar o estoque

--- src/app/pages/acertar-estoque/acertar-estoque.component.ts
+++ src/app/pages/acertar-estoque/acertar-estoque.component.ts
@@ -1,13 +1,154 @@
 import { Component, OnInit } from '@angular/core';
 
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
+import { Validators, FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { FilterService } from 'src/app/shared/services/filter.service';
+import { Racao } from 'src/app/shared/models/racao';
+import { RacaoService } from 'src/app/shared/services/racao.service';
+import { MessageService } from 'primeng/api';
+
 @Component({
   selector: 'app-acertar-estoque',
   templateUrl: './acertar-estoque.component.html',
-  styleUrls: ['./acertar-estoque.component.scss']
+  styleUrls: ['./acertar-estoque.component.scss'],
+  providers: [MessageService]
 })
 export class AcertarEstoqueComponent implements OnInit {
+  currentPage: number = 1;
+  itemsPerPage: number = 6;
+  totalPages: number = 0;
+  displayItems: Racao[] = [];
+  racoes: Racao[] = [];
+  selectedFilter: string = 'nome';
+  searchTerm = new FormControl();
+  isDropdownOpen: boolean = false;
+  visible: boolean = false;
+  idRacao: number = 0;
+  nomeRacao: string = '';
+  formGroup: FormGroup;
+  
+
+  constructor(
+    private racaoService: RacaoService,
+    private filterService: FilterService,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService
+  ) { 
+    this.formGroup = this.formBuilder.group({
+      quantidade: ['', Validators.required]
+    });
+  
+    }
+
 
   ngOnInit(): void {
-      
+    this.getRacoes();
+  }
+
+  getRacoes() {
+    this.filterService.getFilter().pipe(
+      switchMap(({ type, value }) => this.racaoService.getRacoes(type, value)),
+      map((racoes) => {
+        return racoes.map((racao) => {
+          if (!racao.estoque_atual && racao.estoque_atual !== 0) {
+            racao.estoque_atual = 0;
+          }
+          return racao;
+        })
+      })
+
+    ).subscribe((racoes) => {
+      this.racoes = racoes;
+      this.totalPages = Math.ceil(this.racoes.length / this.itemsPerPage);
+      this.updatePage();
+    });
+
+    this.searchTerm.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    )
+      .subscribe((value) => {
+        this.filterService.setFilter(this.selectedFilter, value)
+      })
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePage();
+    }
+  }
+
+  updatePage() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    const itemsToDisplay = this.racoes.slice(startIndex, endIndex);
+    const emptyRowCount = this.itemsPerPage - itemsToDisplay.length;
+
+    const emptyItem: Racao = {
+      id: '',
+      nome: '',
+      id_categoria: '',
+      categoria: '',
+      tipo_racao: '',
+      fase_utilizada: '',
+      batida: '',
+      estoque_minimo: '',
+      estoque_atual: '',
+      ingredientes: []
+    };
+
+    const emptyItems = Array(emptyRowCount).fill(emptyItem);
+    const itemsWithEmptyRows = [...itemsToDisplay, ...emptyItems];
+    this.displayItems = itemsWithEmptyRows;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectFilter(filter: string) {
+    this.selectedFilter = filter;
+    this.filterService.setFilter(filter, this.searchTerm.value);
+    this.isDropdownOpen = false;
+    console.log('Filtro => ', this.selectedFilter);
+  }
+
+  openModalRacao(id: number, name: string) {
+    this.idRacao = id;
+    this.nomeRacao = name;
+    this.visible = true;
+  }
+
+  acertarEstoque() {
+    if (this.formGroup.valid && this.formGroup.value) {
+      const values = {
+        id_racao: +this.idRacao,
+        quantidade: this.formGroup.get('quantidade')?.value
+      }
+      this.racaoService.acertarEstoqueRacao(values).subscribe({
+        next: (res) => {
+          console.log('Acerto de Estoque => ', res);
+          this.formGroup.reset();
+          this.messageService.add({ severity: 'success', summary: 'Estoque Acertado', detail: 'Estoque acertado com sucesso!' });
+          this.getRacoes();
+          setTimeout(() => {
+            this.visible = false;
+          }, 2000)
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Erro', detail: err.error.message || `Ocorreu um erro ao acertar o estoque` });
+          console.error('Erro => ', err)
+        }
+      })
+    }
+
   }
 }
--- src/app/pages/acertar-estoque/acertar-estoque.module.ts
+++ src/app/pages/acertar-estoque/acertar-estoque.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AcertarEstoqueRoutingModule } from './acertar-estoque-routing.module';
 import { AcertarEstoqueComponent } from './acertar-estoque.component';
-
+import { DialogModule } from 'primeng/dialog';
+import { MenuModule } from 'src/app/shared/menu/menu.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,12 @@ import { AcertarEstoqueComponent } from './acertar-estoque.component';
   ],
   imports: [
     CommonModule,
-    AcertarEstoqueRoutingModule
+    AcertarEstoqueRoutingModule,
+    DialogModule,
+    MenuModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule
   ]
 })
 export class AcertarEstoqueModule { }
