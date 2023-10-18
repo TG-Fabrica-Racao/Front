619f517c4463e60865f842fccfdddea867771341
matheusgmm <matheus.gmuccio@gmail.com>
2023-10-18 10:59:39 -0300

    Feat: Criando página histórico rações

---
+++ src/app/pages/sistema/historico-racoes/historico-racoes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historico-racoes',
+  templateUrl: './historico-racoes.component.html',
+  styleUrls: ['./historico-racoes.component.scss']
+})
+export class HistoricoRacoesComponent {
+
+}
