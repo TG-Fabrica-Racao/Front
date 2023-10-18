fb2193a04b38fc8e613d5b036891aa3063bc0273
matheusgmm <matheus.gmuccio@gmail.com>
2023-10-18 09:36:37 -0300

    Feat: Adicioando interface

--- src/app/shared/models/login.ts
+++ src/app/shared/models/login.ts
@@ -7,3 +7,9 @@ export interface LoginResponse {
     message: string;
     token: string;
 }
+
+export interface ChangePassword {
+    email: string;
+    senha_atual: string;
+    senha_nova: string;
+}
