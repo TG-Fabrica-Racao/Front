6081067bfbb46f2e2cda551a5c992216c9b6cc8e
matheusgmm <matheus.gmuccio@gmail.com>
2023-10-18 10:58:46 -0300

    Feat: Criando página acertos de estoque

---
+++ src/app/pages/sistema/acertos-estoque/acertos-estoque.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acertos-estoque',
+  templateUrl: './acertos-estoque.component.html',
+  styleUrls: ['./acertos-estoque.component.scss']
+})
+export class AcertosEstoqueComponent {
+
+}
