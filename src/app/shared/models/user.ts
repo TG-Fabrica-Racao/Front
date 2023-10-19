4702b036c7af574505831d64933945cf9a6f984e
matheusgmm <matheus.gmuccio@gmail.com>
2023-10-19 10:50:57 -0300

    Feat: Criando interfaces do usuário

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,8 +1,23 @@
 export interface User {
-  id: number;
+  id: number | string;
   nome: string;
   email: string;
   telefone: string;
-  status_usuario: number;
+  status_usuario: string;
+  cargo: string;
+}
+
+export interface CreateUser {
+  nome: string;
+  email: string;
+  telefone: string;
+  cargo: string;
+}
+
+export interface UpdateUser {
+  nome: string;
+  email: string;
+  telefone: string;
+  status_usuario: string;
   cargo: string;
 }
