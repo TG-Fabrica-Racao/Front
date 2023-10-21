d500ce3965b1635128a2273f24bbe71b5daaa105
matheusgmm <matheus.gmuccio@gmail.com>
2023-10-20 21:29:09 -0300

    Feat: funções para consultar acertos de estoque

--- src/app/pages/sistema/acertos-estoque/acertos-estoque.component.ts
+++ src/app/pages/sistema/acertos-estoque/acertos-estoque.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { debounceTime } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { LogAcertos } from 'src/app/shared/models/racao';
+import { RacaoService } from 'src/app/shared/services/racao.service';
 
 @Component({
   selector: 'app-acertos-estoque',
   templateUrl: './acertos-estoque.component.html',
   styleUrls: ['./acertos-estoque.component.scss']
 })
-export class AcertosEstoqueComponent {
+export class AcertosEstoqueComponent implements OnInit {
+  currentPage: number = 1;
+  itemsPerPage: number = 6;
+  totalPages: number = 0;
+  displayItems: LogAcertos[] = [];
+  logs: LogAcertos[] = [];
+  formGroup: FormGroup;
 
-}
+  constructor(
+    private racaoService: RacaoService,
+    private formBuilder: FormBuilder
+  ) {  
+    this.formGroup = this.formBuilder.group({
+      nome_racao: [''],
+      data_inicial: [''],
+      data_final: [''],
+    }); 
+  }
+
+
+
+  ngOnInit(): void {
+    this.getLogs();
+    this.formGroup.valueChanges.pipe(
+      debounceTime(300)
+    ).subscribe(() => {
+      this.getLogs();
+    }) 
+  }
+
+  getLogs() {
+    this.racaoService.getAcertosLogs(this.formGroup.value).subscribe({
+      next: (res) => {
+        console.log('Logs => ', res)
+        this.logs = res;
+        this.totalPages = Math.ceil(this.logs.length / this.itemsPerPage);
+        this.updatePage();
+      },
+      error: (erro) => {
+        console.error('Erro => ', erro)
+      }
+    })
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePage();
+    }
+  }
+
+  updatePage() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    const itemsToDisplay = this.logs.slice(startIndex, endIndex);
+    const emptyRowCount = this.itemsPerPage - itemsToDisplay.length;
+
+    const emptyItem: LogAcertos = {
+      id: '',
+      racao: '',
+      data: '',
+      usuario: '',
+      quantidade: '',
+    };
+
+    const emptyItems = Array(emptyRowCount).fill(emptyItem);
+    const itemsWithEmptyRows = [...itemsToDisplay, ...emptyItems];
+    this.displayItems = itemsWithEmptyRows;
+  }
+} 
